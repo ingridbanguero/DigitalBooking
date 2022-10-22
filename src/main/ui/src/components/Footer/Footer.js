6d6369cb99b6746e341bf2a59178190476388f2c
Ingrid Banguero <ingrid_banguero@hotmail.com>
2022-10-21 19:57:10 -0500

    Arreglar estilos buscador

--- src/main/ui/src/components/Footer/Footer.js
+++ src/main/ui/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.scss';
 const Footer = () => {
     return(
         <footer>
-            <p className="text2">©2021 Digital Booking</p>
+            <p>©2021 Digital Booking</p>
             <SocialNetwork/>
         </footer>
     )
