81e2cca60841fcfd5a61305aa37942cfeb3d14d1
Ingrid Banguero <ingrid_banguero@hotmail.com>
2022-10-25 18:09:56 -0500

    Implementar template categorias

--- src/main/ui/src/components/CardCategory/CardCategory.js
+++ src/main/ui/src/components/CardCategory/CardCategory.js
@@ -1,8 +1,14 @@
 import './CardCategory.scss';
 
-const CardCategory = () => { 
+const CardCategory = (props) => { 
     return(
-        <></>
+        <div className='card-category'>
+            <img src={props.details.Imagen} alt="category"/>
+            <div>
+                <h3>{props.details.Categoria}</h3>
+                <h4>807.105 {props.details.Categoria}</h4>
+            </div>
+        </div>
     )
 }
 
--- src/main/ui/src/components/CardCategoryContainer/CardCategoryContainer.js
+++ src/main/ui/src/components/CardCategoryContainer/CardCategoryContainer.js
@@ -1,8 +1,16 @@
 import './CardCategoryContainer.scss';
+import CardCategory from "../CardCategory/CardCategory"
+import Categorias from "../../helpers/categorias.json";
 
 const CardCategoryContainer = () => { 
     return(
-        <></>
+        <div className="card-category-container">
+            <h2>Buscar por tipo de alojamiento</h2>
+            <div>
+            { Categorias.map((item, index) => <CardCategory details={item} key={index}/>)}
+            </div>
+            
+        </div>
     )
 }
 
