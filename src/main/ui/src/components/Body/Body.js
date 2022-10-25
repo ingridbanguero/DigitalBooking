b755cfe4f12ba812f23839b83ad4a9dcdbf6b926
tatiana-zanelli <tatiana.programacion@gmail.com>
2022-10-24 23:17:11 -0300

    Inicio tarea 7

--- src/main/ui/src/components/Body/Body.js
+++ src/main/ui/src/components/Body/Body.js
@@ -1,10 +1,14 @@
 import Search from "../Search/Search";
+import CardListContainer from "../CardListContainer/CardListContainer";
 import './Body.scss';
+
+
 
 const Body = () => {
     return(
         <main>
             <Search/>
+            <CardListContainer/>
         </main>
     )
 }
--- src/main/ui/src/components/CardList/CardList.js
+++ src/main/ui/src/components/CardList/CardList.js
@@ -1,8 +1,39 @@
 import './CardList.scss';
+import data from "../../helpers/alojamientos.json";
+
 
 const CardList = () => { 
+
+  
+
     return(
-        <></>
+        <cardList>        
+
+            <ul>
+                {
+                    data.map((item, index) => 
+                                            
+                        <div key={index} className="conteiner">
+                            <div className="card">
+                                <div className="card-logo" >
+                                    <img src={item.img} width="100%" />
+                                </div>
+                                <div className="card-content">
+                                    <h4>{item.category}</h4>
+                                    <p>{item.location}</p>
+                                    <h1>{item.title}</h1>  
+                                    <p>{item.description}</p>
+                                    <button>Ver más</button>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        
+                    )
+                }
+            </ul> 
+            
+        </cardList>  
     )
 }
 
