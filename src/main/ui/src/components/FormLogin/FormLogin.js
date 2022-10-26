5b7aca9e6f8a3e653b4a67f55684ceb6c91fe5d7
Juan Pablo Riaño <valholl95@gmail.com>
2022-10-26 18:49:20 -0500

    Agrega template base para el login

--- src/main/ui/src/components/FormLogin/FormLogin.js
+++ src/main/ui/src/components/FormLogin/FormLogin.js
@@ -2,7 +2,17 @@ import './FormLogin.scss';
 
 const FormLogin = () => { 
     return(
-        <></>
+        <form className='formlogin'> 
+            <h1>Iniciar Sesión</h1>  
+            <div className="container">  
+                <label>Correo electrónico</label>   
+                <input type="email" placeholder="Ingresa tu correo electrónico" name="email" required/>  
+                <label>Contraseña</label>   
+                <input type="password" placeholder="Ingresa tu contraseña" name="password" required/> 
+                <button type="submit">Ingresar</button>   
+                <p>¿Aún no tienes cuenta? <a href='/register'>Registrate aquí</a></p>    
+            </div>   
+        </form> 
     )
 }
 
