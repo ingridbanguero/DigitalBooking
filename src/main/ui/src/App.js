fcc42f3f8461d514d45baba7240b8eba3c4578b6
danielforozco <daniel.orozco@gmail.com>
2022-10-25 21:20:08 -0500

    evento header

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -1,13 +1,21 @@
 import './App.scss';
 import Home from './pages/Home/Home';
-
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+      <BrowserRouter>
+            <div className="App">
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/login' element={<Login/>} />
+            <Route path='/register' element={<Register/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 
