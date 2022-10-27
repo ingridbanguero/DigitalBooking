ebb83dd317fa73170ee83ecd385c16767456c5eb
Juan Pablo Riaño <valholl95@gmail.com>
2022-10-26 19:32:22 -0500

    Agrega el template base pÃara el registro

--- src/main/ui/src/components/FormRegister/FormRegister.js
+++ src/main/ui/src/components/FormRegister/FormRegister.js
@@ -2,7 +2,23 @@ import './FormRegister.scss';
 
 const FormRegister = () => { 
     return(
-        <></>
+        <form className='formregister'> 
+            <h1>Crear Cuenta</h1>  
+            <div className="container">
+                <label>Nombre</label>   
+                <input type="text" placeholder="Ingresa tu nombre" name="email" required/>
+                <label>Apellido</label>   
+                <input type="text" placeholder="Ingresa tu apellido" name="email" required/>  
+                <label>Correo electrónico</label>   
+                <input type="email" placeholder="Ingresa tu correo electrónico" name="email" required/>  
+                <label>Contraseña</label> 
+                <input type="password" placeholder="Ingresa tu contraseña" name="password" required/>
+                <label>Confirmar contraseña</label>   
+                <input type="password" placeholder="Vuelve a ingresar tu contraseña" name="password" required/>  
+                <button type="submit">Crear cuenta</button>   
+                <p>¿Ya tienes cuenta? <a href='/login'>Iniciar sesión</a></p>
+            </div>   
+        </form> 
     )
 }
 
