9e0f8aeb3d2057be327438b9828c333734db38d5
tatiana-zanelli <tatiana.programacion@gmail.com>
2022-11-03 22:19:27 -0300

    Se agrega configuracion CrossOrigin en los controllers

--- src/main/java/com/example/Grupo4/controller/ProductoController.java
+++ src/main/java/com/example/Grupo4/controller/ProductoController.java
@@ -1,22 +1,16 @@
 package com.example.Grupo4.controller;
 
-import com.example.Grupo4.model.Categoria;
 import com.example.Grupo4.model.Producto;
 import com.example.Grupo4.service.ProductoService;
 import java.util.Collection;
 import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/productos")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT})
 public class ProductoController {
 
   private final ProductoService service;
