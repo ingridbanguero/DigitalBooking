cc10451ecf467dfcca57f6232505e33c0c484a26
Ingrid Banguero <ingrid_banguero@hotmail.com>
2022-11-07 15:48:10 -0500

    feature/sprint2-filtros-35-38

--- src/main/ui/src/components/CardCategory/CardCategory.js
+++ src/main/ui/src/components/CardCategory/CardCategory.js
@@ -3,7 +3,7 @@ import './CardCategory.scss';
 const CardCategory = (props) => { 
 
     return(
-        <div className='card-category'>
+        <div className='card-category' onClick={() => props.onSelectCategory(props.details.id)}>
             <img src={props.details.imagenUrl} alt="category"/>
             <div>
                 <h3>{props.details.titulo}</h3>
